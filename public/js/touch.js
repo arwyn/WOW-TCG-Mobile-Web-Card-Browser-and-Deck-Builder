d4ee8fdebe0fb07c88048f977534d72a4a24beb9
Arwyn Hainsworth <arwynh@gmail.com>
2011-08-26 20:40:35 +0900

    * Updating from site

--- public/js/touch.js
+++ public/js/touch.js
@@ -1,8 +1,20 @@
 $(function() {
+	var p=1, imgs = $(".gallary img"),
+		moveimg = function(i) {
+			if(p+i >= 0 && p+i <imgs.length) {
+				imgs.eq(p).hide();
+				p = p+i;
+				imgs.eq(p).show();
+			}
+		}
+	;
+	imgs.hide();
+	moveimg(0);
+
 	$(".gallary").bind("swipeleft", function(e) {
-		$(".info").text("left");
+		moveimg(1);
 	});
 	$(".gallary").bind("swiperight", function(e) {
-		$(".info").text("right");
+		moveimg(-1);
 	});
 });
