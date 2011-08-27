090b727fb35b8d158a842f5263ec511b2d30a814
Arwyn Hainsworth <arwynh@gmail.com>
2011-08-27 12:48:35 +0900

    Adding static db file to load to client side + modifying code to use internal db

--- public/js/touch.js
+++ public/js/touch.js
@@ -1,20 +1,52 @@
 $(function() {
-	var p=1, imgs = $(".gallary img"),
-		moveimg = function(i) {
-			if(p+i >= 0 && p+i <imgs.length) {
-				imgs.eq(p).hide();
-				p = p+i;
-				imgs.eq(p).show();
+	var c = $(".cards"),
+		d = [["woe",23],["woe",24],["woe",25],["woe",26],["woe",27],["woe",28]],
+		p = 0,
+		loadCards = function(p) {
+			var np, t, s, n;
+			for(np=p-2;np<p+2;np++) {
+				if(np >= 0 && np < d.length) { 
+					s=d[np][0];
+					n=d[np][1];
+					t=$(document.createElement("img"));
+					t.attr("src", "/images/"+s+"/"+internalStorage.card[s][n].image);
+					t.attr("id", "card-"+s+"-"+n);
+					if(np>p) {
+						c.append(t);
+					}
+					else {
+						c.prepend(t);
+					}
+				}
+			}
+		},
+		switchCard = function(dir) {
+			var np = p+dir;
+			if(np >= 0 && np < d.length) {
+				loadCards();
+				$("img#card-"+d[p][0]+d[p][1]).hide();
+				p=np;
+				$("img#card-"+d[p][0]+d[p][1]).show();
 			}
 		}
 	;
-	imgs.hide();
-	moveimg(0);
+	
+	// initialise db
+	if(!internalStorage.card) {
+		$.getJson("http://y-ddraig-goch.net/m/db.json", {}, function(data) {
+			$.extend(internalStorage, data);
+			switchCard(0);
+		});
+	}
+	else {
+		switchCard(0);
+	}
+	
 
-	$(".gallary").bind("swipeleft", function(e) {
-		moveimg(1);
+	$("body").bind("swipeleft", function(e) {
+		switchCard(1);
 	});
-	$(".gallary").bind("swiperight", function(e) {
-		moveimg(-1);
+	$("body").bind("swiperight", function(e) {
+		switchCard(-1);
 	});
 });
