f389be99cbbf7ea33cdea029df9720819e045612
Arwyn Hainsworth <arwynh@gmail.com>
2011-09-25 09:48:56 +0900

    Moved to jquery mobile page system. Added indexs and basic type search.

--- public/js/touch.js
+++ public/js/touch.js
@@ -3,60 +3,171 @@ $(function() {
 		c = $(".cards"),
 		d = [["woe",23],["woe",24],["woe",25],["woe",26],["woe",27],["woe",28]],
 		p = 0,
-		getCardId = function(c) {
-			return "card-" + c[0] + "-" + c[1];
-		},
-		loadCards = function(p) {
-			var np, t, s, n;
-			for(np=p-2;np<p+2;np++) {
-				if(np >= 0 && np < d.length) { 
-					s=d[np][0];
-					n=d[np][1];
-					if($("#"+getCardId(d[np])).length) {
-						continue;
-					}
-					t=$(document.createElement("img"));
-					t.attr("src", "images/"+s+"/"+db.card[s][n].image);
-					t.attr("id", "card-"+s+"-"+n);
-					t.addClass("card");
-					if(np>p) {
-						c.append(t);
-					}
-					else {
-						c.prepend(t);
-					}
-				}
-			}
-		},
-		switchCard = function(dir) {
-			var np = p+dir;
-			if(np >= 0 && np < d.length) {
-				loadCards(np);
-				console.info(p);
-				$("img#"+getCardId(d[p])).hide();
-				p=np;
-				$("img#"+getCardId(d[p])).show();
-			}
-		},
 		loadDb = function(callback) {
 			// for now load db file each time
 			// TODO: use localStorage object to cache results
-			$.getJSON(window.location.href + "db.json", {}, function(data) {
+			$.getJSON(window.location.href.replace(/\/[a-z]*?\.html.*$/, '') + "/db.json", {}, function(data) {
 				db = data;
 				callback();
 			});
 		}
 	;
 	
-	loadDb(function() {
-		$(".viewport .loading").hide();
+	// init page logic
+	// home page
+	$("#home").bind("pageshow", function() { if(!db.card) $.mobile.changePage("#dbloader"); });
+	$("#dbloader").bind("pageshow", function() {
+		var addContinue = function() {
+			var btn=$(document.createElement("a"));
+			btn.attr("href","#home");
+			btn.attr("data-role", "button");
+			btn.attr("data-icon", "check");
+			btn.text("Continue");
+			$("#dbloader div.ui-content").append(btn);
+			$("#dbloader").trigger("create");
+		}
+		if(!db.card) {loadDb(addContinue); }
+		else { addContinue(); }
+	});
+	$("#search").bind("pagebeforeshow", function() {
+		if(!db.index) {
+			$.mobile.changePage("#dbloader");
+			return
+		}
+		
+		var ul, li, uli;
+	
+		if(!$("#search div[data-role='content'] ul").length) {
+			ul = $(document.createElement("ul"));
+			ul.attr("data-role","listview");
+			ul.attr("data-inset","true");
+			if(db.index.type) {
+				li = $(document.createElement("li"));
+				li.text(db.index.type["name"]);
+				uli = $(document.createElement("ul"));
+				uli.attr("data-inset","true");
+				$.each(db.index.type.content, function(i,v) {
+					var li = $(document.createElement("li")),
+						num = $(document.createElement("span")),
+						a = $(document.createElement("a"))
+					;
+					a.text(v["name"]);
+					a.attr("href", "#list");
+					a.bind("click", function() { $("#list").jqmData("cards", v.cards); });
+					li.append(a);
+					num.addClass("ui-li-count");
+					num.text(v.cards.length);
+					li.append(num);
+					uli.append(li);
+				});
+				li.append(uli);
+				ul.append(li);
+			}
+			$("#search div[data-role='content']").append(ul).trigger("create");
+		}
+	});
+	
+	$("#list").bind("pagebeforeshow", function() {
+		if(!db.index) {
+			$.mobile.changePage("#dbloader");
+			return
+		}
+
+		var	cards = $("#list").jqmData("cards"),
+			ul = $("#list ul.card-list");
+		
+		if(!cards) {
+			$.mobile.changePage("#home");
+			return;
+		}
+		
+		ul.html("");
+		$.each(cards, function(i, v) {
+			var li=$(document.createElement("li")),
+				a=$(document.createElement("a"))
+			;
+			
+			a.attr("href","#card");
+			a.text(db.card[v[0]][v[1]]["name"]);
+			a.bind("click", function() {
+				$("#card").jqmData("cards", cards).jqmData("card", i);
+			});
+			li.append(a);
+			ul.append(li)
+		});
+		ul.listview("refresh");
+	});
+	
+	$("#card").bind("pagebeforeshow", function() {
+		if(!db.index) {
+			$.mobile.changePage("#dbloader");
+			return
+		}
+
+		var	cards = $("#card").jqmData("cards"),
+			pos = $("#card").jqmData("card"),
+			view = $("#card div.cards"),
+			getCardId = function(card) {
+				return "card-" + card[0] + "-" + card[1];
+			},
+			loadCards = function(pos) {
+				var preload = 2, newpos, tag, set, num, id;
+				for(newpos = pos - preload; newpos < pos + preload; newpos++) {
+					if(newpos >= 0 && newpos < cards.length) { 
+						set = cards[newpos][0];
+						num = cards[newpos][1];
+						id = getCardId(cards[newpos]);
+						if($("#" + id).length) {
+							continue;
+						}
+						tag=$(document.createElement("img"));
+						tag.attr("src", "images/"+db.card[set][num].image);
+						tag.attr("id", id);
+						tag.addClass("card");
+						if(newpos>pos) {
+							view.append(tag);
+						}
+						else {
+							view.prepend(tag);
+						}
+					}
+				}
+			},
+			switchCard = function(dir) {
+				var newpos = pos + dir;
+				if(newpos >= 0 && newpos < cards.length) {
+					loadCards(newpos);
+					$("img#"+getCardId(cards[pos])).removeClass("active");
+					pos=newpos;
+					$("img#"+getCardId(cards[pos])).addClass("active");
+				}
+			}
+		;
+		
+		if(!cards || p == undefined) {
+			$.mobile.changePage("#home");
+			return;
+		}
+		
+		$("#card").bind("swipeleft", function(e) {
+			switchCard(1);
+		});
+		$("#card").bind("swiperight", function(e) {
+			switchCard(-1);
+		});
+
+		loadCards(p);
 		switchCard(0);
 	});
+//	loadDb(function() {
+//		$(".viewport .loading").hide();
+//		switchCard(0);
+//	});
 
-	$("body").bind("swipeleft", function(e) {
-		switchCard(1);
-	});
-	$("body").bind("swiperight", function(e) {
-		switchCard(-1);
-	});
+//	$("body").bind("swipeleft", function(e) {
+//		switchCard(1);
+//	});
+//	$("body").bind("swiperight", function(e) {
+//		switchCard(-1);
+//	});
 });
